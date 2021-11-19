746434bafb848dc0763e31d80166259529bcc194
wpletcher <wpletcher@companieshouse.gov.uk>
2021-11-19 15:27:19 +0000

    remove env var

--- image-converters/start.sh
+++ image-converters/start.sh
@@ -3,13 +3,13 @@
 cd ${0%/*}
 
 #CHECK: Are there any processes already running?
-PIDLIST=`ps -fu ${ENVNAME} | grep -v grep | grep bulkpdf | awk '{print $2}'`
+PIDLIST=`ps -fu grep -v grep | grep bulkpdf | awk '{print $2}'`
 if [ ! -z ${PIDLIST} ]; then
   echo Processes already running - ${PIDLIST}
   exit 1
 fi
 
-# PIDLIST=`ps -fu ${ENVNAME} | grep -v grep | grep bulkpcl | awk '{print $2}'`
+# PIDLIST=`ps -fu grep -v grep | grep bulkpcl | awk '{print $2}'`
 # if [ ! -z ${PIDLIST} ]; then
 #   echo Processes already running - ${PIDLIST}
 #   exit 1
